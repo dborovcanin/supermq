90b66240dcbecd259c617320876a38b9e636ed55
fatih <fatihdurmaz@sabanciuniv.edu>
2022-05-16 18:26:31 +0300

    Consumer Update
    Signed-off-by: fatih <fatihdurmaz@sabanciuniv.edu>

--- cmd/influxdb-writer/main.go
+++ cmd/influxdb-writer/main.go
@@ -72,8 +72,8 @@ type config struct {
 }
 
 func main() {
-	cfg /*, clientCfg*/ := loadConfigs()
-	print(cfg.dbUrl)
+	cfg, repoCfg := loadConfigs()
+	//print(cfg.dbUrl)
 	println("Hello from influxdb Writer")
 	logger, err := logger.New(os.Stdout, cfg.logLevel)
 	if err != nil {
@@ -92,10 +92,10 @@ func main() {
 		logger.Error(fmt.Sprintf("Failed to create InfluxDB client: %s", err))
 		os.Exit(1)
 	}
-	println("Connected to INFLUXDB2!")
+	println("Connected to InfluxDB")
 	defer client.Close()
 
-	repo := influxdb.New(client, cfg.dbOrg, cfg.dbBucket)
+	repo := influxdb.New(client, repoCfg)
 	counter, latency := makeMetrics()
 	repo = api.LoggingMiddleware(repo, logger)
 	repo = api.MetricsMiddleware(repo, counter, latency)
@@ -129,7 +129,7 @@ func connectToInfluxdb(cfg config) (influxdb2.Client, error) {
 	return client, err
 }
 
-func loadConfigs() config /*influxdata.HTTPConfig*/ {
+func loadConfigs() (config, influxdb.RepoConfig) {
 	cfg := config{
 		brokerURL:  mainflux.Env(envBrokerURL, defBrokerURL),
 		logLevel:   mainflux.Env(envLogLevel, defLogLevel),
@@ -144,7 +144,12 @@ func loadConfigs() config /*influxdata.HTTPConfig*/ {
 		dbToken:    mainflux.Env(envDBToken, defDBToken),
 	}
 	cfg.dbUrl = fmt.Sprintf("http://%s:%s", cfg.dbHost, cfg.dbPort)
-	return cfg //, clientCfg
+
+	repoCfg := influxdb.RepoConfig{
+		Bucket: cfg.dbBucket,
+		Org:    cfg.dbOrg,
+	}
+	return cfg, repoCfg
 }
 
 func makeMetrics() (*kitprometheus.Counter, *kitprometheus.Summary) {
